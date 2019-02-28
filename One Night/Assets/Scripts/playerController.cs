f85426a355a6f4a8a241986fcf2cbe24e9790dfa
Martin <linnos.mg@gmail.com>
2019-02-28 08:36:52 -0500

    Sprint and stamina system added

--- One Night/Assets/Scripts/playerController.cs
+++ One Night/Assets/Scripts/playerController.cs
@@ -5,24 +5,72 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
 	public float speed;
+	public float walkSpeed;
 
 	private Rigidbody2D rb;
 	private Vector2 moveVelocity;
+	private Vector2 lastPos;
+	private float distanceTraveled;
+	public float stamina;
+	private float currStamina;
+	private bool canRun;
+	public float recoverStam;
+
+
     // Start is called before the first frame update
     void Start()
     {
-		rb = GetComponent<Rigidbody2D>();
+			rb = GetComponent<Rigidbody2D>();
+			lastPos = transform.position;
+			walkSpeed = speed;
+			canRun = true;
+			currStamina = stamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-		Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		moveVelocity = moveInput.normalized * speed;
-    }
+			Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+			moveVelocity = moveInput.normalized * walkSpeed;
+		
+			Debug.Log("Stamina Left: " + currStamina);
+
+			if(currStamina <= 0)
+			{
+				canRun = false;
+				currStamina = 1;
+			}
+			else if(currStamina >= stamina)
+			{
+				canRun = true;
+			}
+
+			if(canRun && currStamina < stamina && !Input.GetKey(KeyCode.LeftShift))
+			{
+				currStamina += recoverStam * Time.fixedDeltaTime;
+			}
+			else if(!canRun)
+			{
+				currStamina += recoverStam * Time.fixedDeltaTime;
+			}
+
+			if(Input.GetKey(KeyCode.LeftShift) && canRun)
+			{
+				walkSpeed = speed * 2f;
+				currStamina -= Vector2.Distance(transform.position, lastPos);
+				
+			}
+			else
+			{
+				walkSpeed = speed;
+			}
+
+			lastPos = transform.position;
+			rb.MovePosition (rb.position + moveVelocity * Time.fixedDeltaTime);
+		}
 
 	void FixedUpdate()
 	{
-		rb.MovePosition (rb.position + moveVelocity * Time.fixedDeltaTime);
+
 	}
 }
