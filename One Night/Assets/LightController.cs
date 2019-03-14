e0ebda3d84d27d1bc27005983d81491cc1e4dff4
mgutier9 <mgutier9@uncc.edu>
2019-03-13 21:34:30 -0400

    Flashlight is implemented as well as lightmaps

---
+++ One Night/Assets/LightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float offset;
+    public GameObject light;
+    private bool isOn;
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            light.SetActive(isOn = !isOn);
+        }
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+    }
+ 
+}
