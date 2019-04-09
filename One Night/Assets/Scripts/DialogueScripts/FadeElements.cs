12a116e41d48b023efd9374a8122de9e3894ea16
Audrey <43081195+Audi-Un-Autre@users.noreply.github.com>
2019-04-09 02:50:19 -0400

    BoatHouse Update

--- One Night/Assets/Scripts/DialogueScripts/FadeElements.cs
+++ One Night/Assets/Scripts/DialogueScripts/FadeElements.cs
@@ -15,7 +15,7 @@ public class FadeElements : MonoBehaviour
     public IEnumerator FadeOut(){
         CanvasGroup UI = GetComponentInParent<CanvasGroup>();
         while (UI.alpha > 0f){
-            UI.alpha -= Time.deltaTime / 100f;
+            UI.alpha -= Time.deltaTime / 25f;
             yield return null;
         }
         //UI.interactable = false;
